201bed2d5c6d93ff0b804027a90c6eed1b045eb6
ShenSKY <sky@hi-global.cn>
2018-09-06 13:59:06 +0800

    初次上传，简易Demo

---
+++ PhotoAlbumDemo/PhotoAlbumDemo/ViewController.m
@@ -0,0 +1,83 @@
+//
+//  ViewController.m
+//  PhotoAlbumDemo
+//
+//  Created by 沈凯 on 2018/9/6.
+//  Copyright © 2018年 Ssky. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *image;
+@property (strong, nonatomic) UIImagePickerController *picker;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.picker.delegate = self;
+    self.picker.allowsEditing = YES;
+}
+
+- (IBAction)buttonAction:(UIButton *)sender {
+    
+    BOOL isPicker = NO;
+    
+    switch (sender.tag) {
+        case 10000:
+//            打开相机
+            isPicker = true;
+//            判断相机是否可用
+            if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+                self.picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+                isPicker = true;
+            }
+            break;
+            
+        case 10001:
+//            打开相册
+            self.picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+            isPicker = true;
+            break;
+            
+        default:
+            self.picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+            isPicker = true;
+            break;
+    }
+    
+    if (isPicker) {
+        [self presentViewController:self.picker animated:YES completion:nil];
+    }else {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"错误" message:@"相机不可用" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *action = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:nil];
+        [alert addAction:action];
+        
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+}
+
+- (UIImagePickerController *)picker
+{
+    if (!_picker) {
+        _picker = [[UIImagePickerController alloc]init];
+    }
+    return _picker;
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    self.image.image = image;
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+@end
